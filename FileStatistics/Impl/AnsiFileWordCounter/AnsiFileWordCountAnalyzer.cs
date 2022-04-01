03b7142f02b1a46b16ef18d913ad4a2e9d2a72b2
Alexey Savenko <sav.alex.jit@gmail.com>
2022-04-01 21:52:31 +0300

    Extracted FileAnalyzer class to provide base functionality for async reading of file Refactoring: AnsiFileWordCountAnalyzer - reimplemented, all file reading logic moved to base FileAnalyzer
    Add comments to refactored files

--- FileStatistics/Impl/AnsiFileWordCounter/AnsiFileWordCountAnalyzer.cs
+++ FileStatistics/Impl/AnsiFileWordCounter/AnsiFileWordCountAnalyzer.cs
@@ -3,72 +3,93 @@ using System.Text;
 
 namespace FileStatistics.Impl.AnsiFileWordCounter
 {
-    internal class AnsiFileWordCountAnalyzer : IFileAnalyzer
+    /// <summary>
+    /// Counts word occurrency in provided Ansi text file
+    /// </summary>
+    internal class AnsiFileWordCountAnalyzer : FileAnalyzer
     {
-        private readonly string _fileName;
+        #region Private members
+        /// <summary>
+        /// Current word builder
+        /// </summary>
+        private readonly StringBuilder _wordBuilder;
 
-        public AnsiFileWordCountAnalyzer(string fileName)
+        /// <summary>
+        /// Statistics for all words, found in file
+        /// </summary>
+        private readonly AnsiFileWordCountStatistics _stats;
+
+        /// <summary>
+        /// Checks if character is word separator
+        /// </summary>
+        /// <param name="character"></param>
+        /// <returns></returns>
+        private bool IsWordSeparator(char character)
         {
-            _fileName = fileName;
-        }
-
-        public async Task<IFileStatistics> Analyze()
-        {            
-            var stats = new AnsiFileWordCountStatistics();
-            if (string.IsNullOrEmpty(_fileName))
-            {
-                stats.Summary.Append("File name is not provided!");
-                stats.AnalysisResult = AnalysisResult.ErrorFileNameIsEmptyOrNull;
-                return stats;
-            }
-
-            if(!File.Exists(_fileName))
-            {
-                stats.AnalysisResult = AnalysisResult.ErrorFileNotFound;
-                stats.Summary.Append($"File {_fileName} doesn't exists!");
-                return stats;
-            }
-
-            await using var sourceStream =
-                new FileStream(
-                    _fileName,
-                    FileMode.Open, FileAccess.Read, FileShare.Read,
-                    bufferSize: 4096, useAsync: true
+            return char.IsWhiteSpace(character) || 
+                char.IsSeparator(character) || 
+                (
+                    char.IsPunctuation(character) && 
+                    (character != ':')// it's punctuation char, but it's shouldn't be according functional requirenments
                 );
-
-            var sb = new StringBuilder();
-                        
-            byte[] buffer = new byte[0x1000];
-            int numRead;
-            
-            while ((numRead = await sourceStream.ReadAsync(buffer)) != 0)
-            {
-                for(int i = 0; i<numRead; i++)
-                {
-                    var symb = (char)buffer[i];
-
-
-                    if(char.IsWhiteSpace(symb) || char.IsSeparator(symb) || (char.IsPunctuation(symb)&&(symb!=':')))
-                    {
-                        
-                        var curWord = sb.ToString();
-                        if (string.IsNullOrEmpty(curWord))
-                            continue;
-
-                        stats[curWord] += 1;
-                        sb.Clear();
-                    }
-                    else
-                        sb.Append(symb);                    
-                }                
-            }
-
-            if (sb.Length > 0)
-            {                
-                stats[sb.ToString()] += 1;
-            }
-
-            return stats;
         }
+
+        #endregion
+
+        #region Protected members
+               
+        protected override IFileStatistics GetFileStatistics()
+        {            
+            return _stats;
+        }
+
+        protected override ByteProcessingResult OnProcessNextByte(byte readByte)
+        {
+            //convert read byte to character
+            var character = (char)readByte;
+
+            //if we're on separator
+            if (IsWordSeparator(character))
+            {                
+                //build current word
+                var curWord = _wordBuilder.ToString();
+               
+                //update word occurrence statistics
+                if(curWord.Length > 0)
+                {
+                    _stats[curWord] += 1;
+                    _wordBuilder.Clear();
+                }                   
+            }
+            else//append character to current word
+                _wordBuilder.Append(character);
+
+            return ByteProcessingResult.Continue;
+        }
+        #endregion
+
+        #region Public Members        
+        public AnsiFileWordCountAnalyzer(string fullFilePath) : base(fullFilePath)
+        {
+            _stats = new();
+            _wordBuilder = new();
+        }
+
+        public override async Task<IFileStatistics> Analyze()
+        {            
+            _wordBuilder.Clear();
+            
+            await base.Analyze();
+
+            //build the last word in file
+            var curWord = _wordBuilder.ToString();
+            if (curWord.Length > 0)
+            {
+                _stats[curWord.ToString()] += 1;
+                _wordBuilder.Clear();
+            }            
+            return _stats;
+        }
+        #endregion
     }
 }
---
+++ FileStatistics/Impl/FileAnalyzer.cs
@@ -0,0 +1,101 @@
+﻿using FileStatistics.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileStatistics.Impl
+{
+    /// <summary>
+    /// Base class for all File Analyzers types
+    /// </summary>
+    /// <remarks>
+    /// Provides async reading of specified file.
+    /// All successors of this class should implement methods to provide their own statistics algorythms
+    /// </remarks>
+    internal abstract class FileAnalyzer : IFileAnalyzer
+    {
+        #region Protected members
+        /// <summary>
+        /// Allows successor to control flow of file analysis
+        /// </summary>
+        protected enum ByteProcessingResult
+        {
+            Continue,
+            Stop
+        }
+
+        /// <summary>
+        /// Full path to file to work with
+        /// </summary>
+        protected string FullFilePath { get; }
+
+        /// <summary>
+        /// Creates specific IFileStatistics implementator
+        /// </summary>
+        /// <returns>IFileStatistics implementator that will be returned from Analyze() method</returns>        
+        protected abstract IFileStatistics GetFileStatistics();
+
+        protected abstract ByteProcessingResult OnProcessNextByte(byte readByte);
+
+        #endregion
+
+        #region Public members   
+        public FileAnalyzer(string fullFilePath)
+        {
+            this.FullFilePath = fullFilePath;
+        }
+
+        /// <summary>
+        /// Base async implementation of file analization
+        /// </summary>
+        /// <returns>Result of analyzis</returns>
+        /// <exception cref="ArgumentNullException">If CreateFileStatistics() doen't return FileStatistics successor</exception>
+        public virtual async Task<IFileStatistics> Analyze()
+        {
+            //Analyze method should always return a value
+            if (GetFileStatistics() is not FileStatistics stats)
+                throw new ArgumentNullException("Method CreateFileStatistics must return FileStatistics successor!");
+
+            if (string.IsNullOrEmpty(FullFilePath))
+            {
+                stats.Summary.Append("File name is not provided!");
+                stats.AnalysisResult = AnalysisResult.ErrorFileNameIsEmptyOrNull;
+                return stats;
+            }
+
+            if (!File.Exists(FullFilePath))
+            {
+                stats.AnalysisResult = AnalysisResult.ErrorFileNotFound;
+                stats.Summary.Append($"File {FullFilePath} doesn't exists!");
+                return stats;
+            }
+
+            //open stream to file
+            await using var sourceStream =
+                new FileStream(
+                    FullFilePath,
+                    FileMode.Open, FileAccess.Read, FileShare.Read,
+                    bufferSize: 4096, useAsync: true
+                );                        
+
+            byte[] buffer = new byte[0x1000];
+            int numRead;
+            //async reading portion of bytes from file
+            while ((numRead = await sourceStream.ReadAsync(buffer)) != 0)
+            {                
+                //process each read byte 
+                for (int i = 0; i < numRead; i++)
+                {
+                    //successor can stop the analysis
+                    if (OnProcessNextByte(buffer[i]) == ByteProcessingResult.Stop)
+                        return stats;
+                }
+            }            
+
+            return stats;
+        }
+        #endregion
+    }
+}
