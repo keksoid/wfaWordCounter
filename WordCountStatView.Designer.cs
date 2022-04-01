d7884676440c4ec8d23b85da286d212573ddb812
Alexey Savenko <savenko.alexey.valerevich@gmail.com>
2022-04-01 18:09:54 +0300

    Designed simple ui with commands to open and process file using async file word counter. Added support to virtual mode for statistics list view. Added sorting support by column clicking

--- WordCountStatView.Designer.cs
+++ WordCountStatView.Designer.cs
@@ -28,46 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lvWordCount = new System.Windows.Forms.ListView();
+            this.colWord = new System.Windows.Forms.ColumnHeader();
+            this.colWordCount = new System.Windows.Forms.ColumnHeader();
+            this.msMainMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.msiAnalyzeFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.msiExitApp = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.lblAllWordCount = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.msMainMenu.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // tableLayoutPanel1
             // 
-            this.button1.Location = new System.Drawing.Point(190, 101);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29.33673F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70.66327F));
+            this.tableLayoutPanel1.Controls.Add(this.lvWordCount, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblAllWordCount, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 93.66391F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.336088F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 363);
+            this.tableLayoutPanel1.TabIndex = 2;
             // 
-            // label1
+            // lvWordCount
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(58, 52);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.lvWordCount.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colWord,
+            this.colWordCount});
+            this.tableLayoutPanel1.SetColumnSpan(this.lvWordCount, 2);
+            this.lvWordCount.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvWordCount.Location = new System.Drawing.Point(3, 3);
+            this.lvWordCount.Name = "lvWordCount";
+            this.lvWordCount.Size = new System.Drawing.Size(778, 334);
+            this.lvWordCount.TabIndex = 0;
+            this.lvWordCount.UseCompatibleStateImageBehavior = false;
+            this.lvWordCount.View = System.Windows.Forms.View.Details;
+            this.lvWordCount.VirtualMode = true;
+            this.lvWordCount.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.lvWordCount_ColumnClick);
+            this.lvWordCount.RetrieveVirtualItem += new System.Windows.Forms.RetrieveVirtualItemEventHandler(this.lvWordCount_RetrieveVirtualItem);
+            // 
+            // colWord
+            // 
+            this.colWord.Text = "Word";
+            this.colWord.Width = 150;
+            // 
+            // colWordCount
+            // 
+            this.colWordCount.Text = "Occurrence";
+            this.colWordCount.Width = 640;
+            // 
+            // msMainMenu
+            // 
+            this.msMainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.msMainMenu.Location = new System.Drawing.Point(0, 0);
+            this.msMainMenu.Name = "msMainMenu";
+            this.msMainMenu.Size = new System.Drawing.Size(784, 24);
+            this.msMainMenu.TabIndex = 3;
+            this.msMainMenu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.msiAnalyzeFile,
+            this.msiExitApp});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // msiAnalyzeFile
+            // 
+            this.msiAnalyzeFile.Name = "msiAnalyzeFile";
+            this.msiAnalyzeFile.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.msiAnalyzeFile.Size = new System.Drawing.Size(230, 22);
+            this.msiAnalyzeFile.Text = "Open file to analyze...";
+            this.msiAnalyzeFile.Click += new System.EventHandler(this.msiAnalyzeFile_Click);
+            // 
+            // msiExitApp
+            // 
+            this.msiExitApp.Name = "msiExitApp";
+            this.msiExitApp.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.msiExitApp.Size = new System.Drawing.Size(230, 22);
+            this.msiExitApp.Text = "Exit";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "Ansi text files(*.txt)|*.txt";
+            this.openFileDialog.InitialDirectory = "C:\\";
+            this.openFileDialog.RestoreDirectory = true;
+            this.openFileDialog.ShowReadOnly = true;
+            // 
+            // lblAllWordCount
+            // 
+            this.lblAllWordCount.AutoSize = true;
+            this.lblAllWordCount.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblAllWordCount.Location = new System.Drawing.Point(3, 340);
+            this.lblAllWordCount.Name = "lblAllWordCount";
+            this.lblAllWordCount.Size = new System.Drawing.Size(223, 23);
+            this.lblAllWordCount.TabIndex = 2;
+            this.lblAllWordCount.Text = "All word count:";
+            this.lblAllWordCount.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // WordCountStatView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(784, 387);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.msMainMenu);
+            this.MainMenuStrip = this.msMainMenu;
             this.Name = "WordCountStatView";
-            this.Text = "Form1";
+            this.Text = "WordCounter";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.msMainMenu.ResumeLayout(false);
+            this.msMainMenu.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private Button button1;
-        private Label label1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private ListView lvWordCount;
+        private ColumnHeader colWord;
+        private ColumnHeader colWordCount;
+        private MenuStrip msMainMenu;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem msiAnalyzeFile;
+        private ToolStripMenuItem msiExitApp;
+        private OpenFileDialog openFileDialog;
+        private Label lblAllWordCount;
     }
 }
--- WordCountStatView.cs
+++ WordCountStatView.cs
@@ -1,20 +1,86 @@
 using FileStatistics.Impl.AnsiFileWordCounter;
+using FileStatistics.Interfaces.AnsiFileWordCounter;
 
 namespace wfaWordCounter
 {
     public partial class WordCountStatView : Form
     {
+        private readonly List<ListViewItem> _viewItemsCache = new();
+
+        private int _sortColIdx = 1;
+        private SortOrder _sortDir = SortOrder.Descending;
+
+        private int CompareWordStats(ListViewItem left, ListViewItem right)
+        {
+            switch (_sortColIdx)
+            {
+                case 0: 
+                    if(_sortDir == SortOrder.Ascending)
+                        return string.Compare(left.Text, right.Text, StringComparison.CurrentCulture);
+                    else
+                        return string.Compare(right.Text, left.Text, StringComparison.CurrentCulture);
+                case 1:
+                    if (_sortDir == SortOrder.Ascending)
+                        return int.Parse(left.SubItems[1].Text) - int.Parse(right.SubItems[1].Text);
+                    else
+                        return int.Parse(right.SubItems[1].Text) - int.Parse(left.SubItems[1].Text);
+                default: 
+                    return 0;
+            }
+        }
+
         public WordCountStatView()
         {
             InitializeComponent();
         }
-
-        private async void button1_Click(object sender, EventArgs e)
+        
+        private async void msiAnalyzeFile_Click(object sender, EventArgs e)
         {
+            if (openFileDialog.ShowDialog() != DialogResult.OK)
+                return;
+            if(string.IsNullOrEmpty(openFileDialog.FileName)||(!File.Exists(openFileDialog.FileName)))
+                return;
+
             var factory = new AnsiFileWordCountAnalyzerFactory();
-            var fileAnalyzer = factory.GetAnalizer("test.txt");
-            var stat = await fileAnalyzer.Analize();
-            label1.Text = stat.GetSummary();
+            var fileAnalyzer = factory.GetAnalyzer(openFileDialog.FileName);
+            
+            if (await fileAnalyzer.Analyze() is not IAnsiFileWordCountStatistics stat)
+                return;
+
+            _viewItemsCache.Clear();
+            lvWordCount.VirtualListSize = 0;
+
+            lblAllWordCount.Text = $"All word Count: {stat.Count}";
+            stat.Words.ToList().ForEach(
+                word =>
+                {
+                    var lvItem = new ListViewItem(word);
+                    lvItem.SubItems.Add(stat[word].ToString());
+                    _viewItemsCache.Add(lvItem);
+                });
+
+            _viewItemsCache.Sort(CompareWordStats);
+
+            lvWordCount.VirtualListSize = _viewItemsCache.Count;
+        }
+
+        private void lvWordCount_RetrieveVirtualItem(object sender, RetrieveVirtualItemEventArgs e)
+        {
+            if (e.ItemIndex >= 0 && e.ItemIndex < _viewItemsCache.Count)
+                e.Item = _viewItemsCache[e.ItemIndex];
+            else
+                e.Item = null;
+        }
+
+        private void lvWordCount_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            if (e.Column == _sortColIdx)
+                _sortDir = _sortDir == SortOrder.Ascending? SortOrder.Descending : SortOrder.Ascending;
+            else
+                _sortColIdx = _sortColIdx == 0 ? 1 : 0;
+
+            _viewItemsCache.Sort(CompareWordStats);
+            lvWordCount.Refresh();
         }
     }
 }
